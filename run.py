bbf9b4260610358db9ae897704c059e03fa43ca6
SeanTangney <sean_tangney@hotmail.com>
2022-01-18 15:49:19 +0000

    Display the board without square brackets and quotation marks

--- run.py
+++ run.py
@@ -1,11 +1,15 @@
 board = []
 for n in range(5):
-    board.append(["0"] * 5)
+    board.append(["O"] * 5)
+
+"""
+Displays the board row by row
+"""
 
 
 def display_board(board):
     for row in board:
-        print(row)
+        print(" ".join(row))
 
 
 display_board(board)
