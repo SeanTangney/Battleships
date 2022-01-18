0be3a8814368ca015adeb9f5fce6a9f62f2eb512
SeanTangney <sean_tangney@hotmail.com>
2022-01-18 15:31:02 +0000

    Add function to display basic board

--- run.py
+++ run.py
@@ -1,3 +1,11 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+board = []
+for n in range(5):
+    board.append(["0"] * 5)
+
+
+def display_board(board):
+    for row in board:
+        print(row)
+
+
+display_board(board)
