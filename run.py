1bd5804c8c2e86b0c21b5dc43b58847a4eb85959
SeanTangney <sean_tangney@hotmail.com>
2022-01-19 21:10:07 +0000

    Find random columns and rows

--- run.py
+++ run.py
@@ -32,11 +32,34 @@ def player_ship(board):
             player_ship_col = int(
                 input("Enter your ship's column: "))
             if (player_ship_col not in range(1, len(board) +1)):
-                 print("Invalid. Enter a number from 1 - 5").format(len(board) + 1)
+                print("Invalid. Enter a number from 1 - 5").format(len(board) + 1)
             else:
                 valid_col = True
         except ValueError:
-            print("Not an valid number, please try again")    
+            print("Not an valid number, please try again")  #
+    return board  
+
+
+# find random row
+def random_row(board):
+    return radint(0, len(board) - 1)
+
+
+# find random column
+def random_col(board):
+    return radint(0, len(board) - 1)
+
+
+
+# randomize the comp ships 
+def comp_ship(board):
+    valid_comp_location = False
+    while(valid_comp_location == False):
+        comp_ship_row = random_row(board)
+        comp_ship_col = random_col(board)
+        if(board[comp_ship_row][comp_ship_col] != "#"):
+            valin_comp_location = True
+    return comp_ship_row, comp_ship_col
 
 
 # Main function to run the game
