c7eee43841d7ef31b8a1aa1e12dfaa4e7334ea85
SeanTangney <sean_tangney@hotmail.com>
2022-01-20 23:14:44 +0000

    Fix errors and change variable scope

--- run.py
+++ run.py
@@ -1,72 +1,88 @@
-from random import randint
+"""
+DOcsting
+"""
+import random
+import shutil
 
+SCREEN_WIDTH = shutil.get_terminal_size().columns
 
 board = []
 for n in range(5):
-    board.append(["O"] * 5)
+    board.append([" 0"] * 5)
 
 
-# Display board in the terminal
 def display_board(board):
+    """
+    Display board in the terminal
+    """
+
     for row in board:
-        print(" ".join(row))
+        print((" ").join(row))
 
 
-# User defines where they put their ships
 def player_ship(board):
+    """
+    User defines where they put their ships
+    """
     valid_row = False
     valid_col = False
-    print("\nEnter your ships co-ordinates..")
-    while valid_row == False:
+    print("Enter your ships co-ordinates..")
+    while not valid_row:
         try:
-            player_ship_row = int(
-                input("Enter your ship's row: "))
-            if (player_ship_row not in range(1, len(board) + 1)):
-                print("Invalid. Enter a number from 1 - 5").format(len(board) + 1)
+            player_ship_row = int(input("Enter your ship's row: "))
+            if player_ship_row not in range(1, len(board) + 1):
+                print("Invalid. Enter a number from 1 - 5")
             else:
                 valid_row = True
         except ValueError:
             print("Not an valid number, please try again")
-    while valid_col == False:
+    while not valid_col:
         try:
-            player_ship_col = int(
-                input("Enter your ship's column: "))
-            if (player_ship_col not in range(1, len(board) +1)):
-                print("Invalid. Enter a number from 1 - 5").format(len(board) + 1)
+            player_ship_col = int(input("Enter your ship's column: "))
+            if player_ship_col not in range(1, len(board) + 1):
+                print("Invalid. Enter a number from 1 - 5")
             else:
+                board[player_ship_row - 1][player_ship_col - 1] = "*"
                 valid_col = True
         except ValueError:
-            print("Not an valid number, please try again")  #
-    return board  
+            print("Not an valid number, please try again")
+
+    return board
 
 
-# find random row
 def random_row(board):
-    return radint(0, len(board) - 1)
+    """
+    find random row
+    """
+    return random.randint(0, len(board) - 1)
 
 
-# find random column
 def random_col(board):
-    return radint(0, len(board) - 1)
+    """
+    find random column
+    """
+    return random.randint(0, len(board) - 1)
 
 
-
-# randomize the comp ships 
 def comp_ship(board):
+    """
+    randomize the comp ships
+    """
     valid_comp_location = False
-    while(valid_comp_location == False):
+    while not valid_comp_location:
         comp_ship_row = random_row(board)
         comp_ship_col = random_col(board)
-        if(board[comp_ship_row][comp_ship_col] != "#"):
-            valin_comp_location = True
+        if board[comp_ship_row][comp_ship_col] != "#":
+            valid_comp_location = True
     return comp_ship_row, comp_ship_col
 
 
-# Main function to run the game
 def main():
+    """
+    Main function to run the game
+    """
     display_board(board)
     player_ship(board)
 
 
 main()
-
